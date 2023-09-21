0e53be29a9ae8519bfb11ae1544895e7a4a6d6a2
shengkai07115 <144308855+shengkai07115@users.noreply.github.com>
2023-09-21 10:58:37 +0800

    Add files via upload

---
+++ lab3-3/lab3-3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int v1 = pow(2,31)-1;
+	char v2 = 'A';
+	bool v3 = true;
+	float v4 = exp(1);
+	double v5 = exp(1);
+	cout << "v1(int)" << v1 << endl;
+	cout << "v2(char)" << v2 << endl;
+	cout << "v3(bool)" << v3 << endl;
+	cout << "v4(float)" << v4 << endl;
+	cout << "v5(double)" << v5 << endl;
+	system("Pause");
+}
