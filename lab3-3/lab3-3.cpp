742905418c2a2c04c5703626c99b4b7ccb39f311
shengkai07115 <144308855+shengkai07115@users.noreply.github.com>
2023-09-21 11:20:56 +0800

    Update lab3-3.cpp

--- lab3-3/lab3-3.cpp
+++ lab3-3/lab3-3.cpp
@@ -9,10 +9,12 @@ int main() {
 	bool v3 = true;
 	float v4 = exp(1);
 	double v5 = exp(1);
+	
 	cout << "v1(int)" << v1 << endl;
 	cout << "v2(char)" << v2 << endl;
 	cout << "v3(bool)" << v3 << endl;
 	cout << "v4(float)" << v4 << endl;
 	cout << "v5(double)" << v5 << endl;
+	cout.precision(16);
 	system("Pause");
 }
